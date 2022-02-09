e24575c69ff6b87869687c77ccdc6d0cdc6e2135
Maciej Hedloff <mhedloff@gmail.com>
2022-02-09 12:18:41 +0100

    working on it

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan'
+import * as process from 'process';
 
-const PORT_NUMBER: number = 3000;
+const PORT_NUMBER: number = process.env.PORT ? +process.env.PORT : 3000;
 
 const app = express();
+
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/hello', (req: Request, res: Response, next: NextFunction) => {
 	return res.status(200).json({
