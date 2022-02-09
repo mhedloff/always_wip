bde24326710cde418706a321a8bc17481080cbcc
Maciej Hedloff <mhedloff@gmail.com>
2022-02-09 11:58:16 +0100

    added Procfile

--- index.ts
+++ index.ts
@@ -4,12 +4,7 @@ const PORT_NUMBER: number = 3000;
 
 const app = express();
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-	console.log(new Date().toISOString(), req.method, req.baseUrl);
-	next()
-})
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get('/hello', (req: Request, res: Response, next: NextFunction) => {
 	return res.status(200).json({
 		success: true
 	})
