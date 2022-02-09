c260147c141f523a5c835994d1802c8418f62fc2
Maciej Hedloff <mhedloff@gmail.com>
2022-02-09 11:34:34 +0100

    ts server

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log('hello');
+import express, { NextFunction, Request, Response } from 'express';
+
+const PORT_NUMBER: number = 3000;
+
+const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	console.log(new Date().toISOString(), req.method, req.baseUrl);
+	next()
+})
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	return res.status(200).json({
+		success: true
+	})
+})
+
+app.listen(PORT_NUMBER, () => {
+	console.log(`app listening on ${PORT_NUMBER}`);
+})
